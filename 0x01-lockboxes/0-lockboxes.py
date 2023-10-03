dbfe558c56903fc9ec8da8e58b24ab6f8dd3c029
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-10-03 19:57:35 +0300

    Format 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+
 def canUnlockAll(boxes):
-    #Initialize a list to keep track of visited boxes.
+    # Initialize a list to keep track of visited boxes.
     visited = [False] * len(boxes)
     # Mark the first box as visited since it's already unlocked
     visited[0] = True
 
-    #initialize a stack for for DFS and add the first box to it.
+    # Initialize a stack for DFS and add the first box to it.
     stack = [0]
 
     while stack:
         current_box = stack.pop()
 
-        #Iterate through the keys in the current box
+        # Iterate through the keys in the current box
         for key in boxes[current_box]:
-            #Check if the key opens a box and if not visited
+            # Check if the key opens a box and if not visited
             if key < len(boxes) and not visited[key]:
                 visited[key] = True
                 stack.append(key)
 
-    #Check if all boxes have been visited.
+    # Check if all boxes have been visited.
     return all(visited)
