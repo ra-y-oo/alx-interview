ce1fada7170d10ac1a013063c2d080813da1aa57
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-10-17 07:11:43 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,54 @@
+#!/usr/bin/python3
+
 import sys
 
-# Define the metrics to compute
+
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_file_size = 0
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
-}
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
-# Read stdin line by line
-line_count = 0
-for line in sys.stdin:
-    line_count += 1
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-    # Split the line into fields
-    fields = line.split()
+        if len(parsed_line) > 2:
+            counter += 1
 
-    # Skip invalid lines
-    if len(fields) != 5:
-        continue
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-    # Parse the fields
-    ip_address = fields[0]
-    date = fields[1]
-    method = fields[2]
-    uri = fields[3]
-    status_code = fields[4]
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-    # Validate the status code
-    try:
-        status_code = int(status_code)
-    except ValueError:
-        continue
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
-    # Increment the status code count
-    status_code_counts[status_code] += 1
-
-    # Compute the total file size
-    file_size = int(fields[4])
-    total_file_size += file_size
-
-    # Print statistics every 10 lines and/or a keyboard interruption (CTRL + C)
-    if line_count % 10 == 0 or sys.stdin.isatty():
-        print("Total file size: File size:", total_file_size)
-        for status_code in sorted(status_code_counts):
-            count = status_code_counts[status_code]
-            if count > 0:
-                print(f"{status_code}: {count}")
-
+finally:
+    print_msg(dict_sc, total_file_size)
